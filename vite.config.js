2ab913ef37bde90f072e3fefd4d9881a6ae94578
kinsa24 <muhkinsa081924@gmail.com>
2026-02-22 01:06:36 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['some-server-only-package']
+    }
+  }
+});
